6a68449bfb24dff39e9bee23b20957d059514cbb
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-21 19:53:54 +0200

    Moved logic for getting necessary tokens for an `impl` clause on a coordinate type into it's own function

--- src/lib.rs
+++ src/lib.rs
@@ -23,9 +23,8 @@ pub fn coord_negate_derive(input: TokenStream) -> TokenStream {
 pub fn coordinate_trait_derive(input: TokenStream) -> TokenStream {
     let ast: syn::DeriveInput = syn::parse(input).unwrap();
 
-    let name: &syn::Ident = &ast.ident;
-    let (impl_generics, type_generics, where_clause) = ast.generics.split_for_impl();
-    let generic = ast.generics.params.first().unwrap();
+    let (name, impl_generics, type_generics, where_clause, generic) =
+        split_for_impl_coordinate_type(&ast);
 
     let gen = quote::quote! {
         impl #impl_generics CoordinateTrait for #name #type_generics #where_clause {
@@ -43,4 +42,20 @@ pub fn coordinate_trait_derive(input: TokenStream) -> TokenStream {
     };
 
     gen.into()
+}
+
+fn split_for_impl_coordinate_type(
+    ast: &syn::DeriveInput,
+) -> (
+    &syn::Ident,
+    syn::ImplGenerics,
+    syn::TypeGenerics,
+    Option<&syn::WhereClause>,
+    &syn::GenericParam,
+) {
+    let name = &ast.ident;
+    let (impl_generics, type_generics, where_clause) = ast.generics.split_for_impl();
+    let generic = ast.generics.params.first().unwrap();
+
+    (name, impl_generics, type_generics, where_clause, generic)
 }
