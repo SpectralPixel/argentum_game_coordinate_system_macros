aa40552798acecf936136c7e032b0550f0eb0cd0
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-21 13:14:18 +0200

    Implemented derive macro which implements `CoordinateTrait`

--- src/lib.rs
+++ src/lib.rs
@@ -18,3 +18,29 @@ pub fn coord_negate_derive(input: TokenStream) -> TokenStream {
 
     gen.into()
 }
+
+#[proc_macro_derive(Coordinate)]
+pub fn coordinate_trait_derive(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+
+    let name: &syn::Ident = &ast.ident;
+    let (impl_generics, type_generics, where_clause) = ast.generics.split_for_impl();
+    let generic = ast.generics.params.first().unwrap();
+
+    let gen = quote::quote! {
+        impl #impl_generics CoordinateTrait for #name #type_generics #where_clause {
+            type Type = #name #type_generics;
+            type FieldType = #generic;
+
+            fn new(x: Self::FieldType, y: Self::FieldType, z: Self::FieldType) -> Self::Type {
+                Self::Type { x, y, z }
+            }
+
+            fn splat(n: Self::FieldType) -> Self::Type {
+                Self::new(n, n, n)
+            }
+        }
+    };
+
+    gen.into()
+}
