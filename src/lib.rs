ec6427c370a04e7cc0f5e96b109721b9cc3e36ae
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-24 13:01:40 +0200

    Ported basic arithmetic from `argentum_game_coordinate_system::macros` to `CoordinateArithmetic`

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 pub fn coord_negate_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast: syn::DeriveInput = syn::parse(input).unwrap();
 
-    let (name, impl_generics, type_generics, where_clause, _generic) =
+    let (name, impl_generics, type_generics, where_clause, generic) =
         split_for_impl_coordinate_type(&ast);
 
     let mut signed_extras = proc_macro2::TokenStream::new();
@@ -22,6 +22,78 @@ pub fn coord_negate_derive(input: proc_macro::TokenStream) -> proc_macro::TokenS
 
     let gen = quote::quote! {
         #signed_extras
+
+        impl #impl_generics std::ops::Add for #name #type_generics #where_clause {
+            type Output = #name #type_generics;
+
+            fn add(self, rhs: #name #type_generics) -> Self::Output {
+                let panic_if_out_of_bounds = || panic!("{} is experiencing integer overflow after adding by {}.", self, rhs);
+                let x = #generic::checked_add(&self.x, &rhs.x).unwrap_or_else(panic_if_out_of_bounds);
+                let y = #generic::checked_add(&self.y, &rhs.y).unwrap_or_else(panic_if_out_of_bounds);
+                let z = #generic::checked_add(&self.z, &rhs.z).unwrap_or_else(panic_if_out_of_bounds);
+                Self::new(x, y, z)
+            }
+        }
+
+        impl #impl_generics std::ops::AddAssign for #name #type_generics #where_clause {
+            fn add_assign(&mut self, rhs: #name #type_generics) {
+                *self = self.to_owned() + rhs;
+            }
+        }
+
+        impl #impl_generics std::ops::Sub for #name #type_generics #where_clause {
+            type Output = #name #type_generics;
+
+            fn sub(self, rhs: #name #type_generics) -> Self::Output {
+                let panic_if_out_of_bounds = || panic!("{} is experiencing integer overflow after subtracting by {}.", self, rhs);
+                let x = #generic::checked_sub(&self.x, &rhs.x).unwrap_or_else(panic_if_out_of_bounds);
+                let y = #generic::checked_sub(&self.y, &rhs.y).unwrap_or_else(panic_if_out_of_bounds);
+                let z = #generic::checked_sub(&self.z, &rhs.z).unwrap_or_else(panic_if_out_of_bounds);
+                Self::new(x, y, z)
+            }
+        }
+
+        impl #impl_generics std::ops::SubAssign for #name #type_generics #where_clause {
+            fn sub_assign(&mut self, rhs: #name #type_generics) {
+                *self = self.to_owned() - rhs;
+            }
+        }
+
+        impl #impl_generics std::ops::Mul for #name #type_generics #where_clause {
+            type Output = #name #type_generics;
+
+            fn mul(self, rhs: #name #type_generics) -> Self::Output {
+                let panic_if_out_of_bounds = || panic!("{} is experiencing integer overflow after multiplying by {}.", self, rhs);
+                let x = #generic::checked_mul(&self.x, &rhs.x).unwrap_or_else(panic_if_out_of_bounds);
+                let y = #generic::checked_mul(&self.y, &rhs.y).unwrap_or_else(panic_if_out_of_bounds);
+                let z = #generic::checked_mul(&self.z, &rhs.z).unwrap_or_else(panic_if_out_of_bounds);
+                Self::new(x, y, z)
+            }
+        }
+
+        impl #impl_generics std::ops::MulAssign for #name #type_generics #where_clause {
+            fn mul_assign(&mut self, rhs: #name #type_generics) {
+                *self = self.to_owned() * rhs;
+            }
+        }
+
+        impl #impl_generics std::ops::Div for #name #type_generics #where_clause {
+            type Output = #name #type_generics;
+
+            fn div(self, rhs: #name #type_generics) -> Self::Output {
+                let panic_if_out_of_bounds = || panic!("{} encountered a problem while being divided by {}.", self, rhs);
+                let x = #generic::checked_div(&self.x, &rhs.x).unwrap_or_else(panic_if_out_of_bounds);
+                let y = #generic::checked_div(&self.y, &rhs.y).unwrap_or_else(panic_if_out_of_bounds);
+                let z = #generic::checked_div(&self.z, &rhs.z).unwrap_or_else(panic_if_out_of_bounds);
+                Self::new(x, y, z)
+            }
+        }
+
+        impl #impl_generics std::ops::DivAssign for #name #type_generics #where_clause {
+            fn div_assign(&mut self, rhs: #name #type_generics) {
+                *self = self.to_owned() / rhs;
+            }
+        }
     };
 
     gen.into()
