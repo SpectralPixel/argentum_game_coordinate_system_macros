781120e3aef59d1073199d5c66771fd0d795d17d
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-27 16:37:07 +0200

    Created `arithmetic` module

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+mod arithmetic;
+
 #[proc_macro_derive(Coordinate, attributes(signed))]
 pub fn coordinate_trait_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast: syn::DeriveInput = syn::parse(input).unwrap();
-    
+
     let name = &ast.ident;
     let (impl_generics, type_generics, where_clause) = ast.generics.split_for_impl();
     let generic = ast.generics.params.first().unwrap();
