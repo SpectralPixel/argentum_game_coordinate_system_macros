e2b5cc0b9409a4f2560b43e2d8ee91d0e911c05c
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-27 19:28:34 +0200

    Moved code around within `arithmetic` to prepare for the next step Also, some variable names within `generate` have been shortened to prevent `cargo fmt` from splitting lines

--- src/arithmetic.rs
+++ src/arithmetic.rs
@@ -2,30 +2,6 @@ use proc_macro2::TokenStream;
 use quote::quote;
 use syn::{DeriveInput, GenericParam, Ident, ImplGenerics, TypeGenerics, WhereClause};
 
-fn neg(
-    ast: &DeriveInput,
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-) -> Option<TokenStream> {
-    match ast.attrs.get(0) {
-        Some(attr) => match attr.path().is_ident("signed") {
-            true => Some(quote! {
-                impl #impl_generics std::ops::Neg for #name #type_generics #where_clause {
-                    type Output = #name #type_generics;
-
-                    fn neg(self) -> Self::Output {
-                        Self::new(-self.x, -self.y, -self.z)
-                    }
-                }
-            }),
-            false => None,
-        },
-        None => None,
-    }
-}
-
 fn add(
     name: &Ident,
     impl_generics: &ImplGenerics,
@@ -43,21 +19,6 @@ fn add(
                 let y = #generic::checked_add(&self.y, &rhs.y).unwrap_or_else(panic_if_out_of_bounds);
                 let z = #generic::checked_add(&self.z, &rhs.z).unwrap_or_else(panic_if_out_of_bounds);
                 Self::new(x, y, z)
-            }
-        }
-    }
-}
-
-fn add_assign(
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-) -> TokenStream {
-    quote! {
-        impl #impl_generics std::ops::AddAssign for #name #type_generics #where_clause {
-            fn add_assign(&mut self, rhs: #name #type_generics) {
-                *self = self.to_owned() + rhs;
             }
         }
     }
@@ -85,21 +46,6 @@ fn sub(
     }
 }
 
-fn sub_assign(
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-) -> TokenStream {
-    quote! {
-        impl #impl_generics std::ops::SubAssign for #name #type_generics #where_clause {
-            fn sub_assign(&mut self, rhs: #name #type_generics) {
-                *self = self.to_owned() - rhs;
-            }
-        }
-    }
-}
-
 fn mul(
     name: &Ident,
     impl_generics: &ImplGenerics,
@@ -117,21 +63,6 @@ fn mul(
                 let y = #generic::checked_mul(&self.y, &rhs.y).unwrap_or_else(panic_if_out_of_bounds);
                 let z = #generic::checked_mul(&self.z, &rhs.z).unwrap_or_else(panic_if_out_of_bounds);
                 Self::new(x, y, z)
-            }
-        }
-    }
-}
-
-fn mul_assign(
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-) -> TokenStream {
-    quote! {
-        impl #impl_generics std::ops::MulAssign for #name #type_generics #where_clause {
-            fn mul_assign(&mut self, rhs: #name #type_generics) {
-                *self = self.to_owned() * rhs;
             }
         }
     }
@@ -159,16 +90,51 @@ fn div(
     }
 }
 
-fn div_assign(
+fn bitand(
+    name: &Ident,
+    impl_generics: &ImplGenerics,
+    type_generics: &TypeGenerics,
+    where_clause: &Option<&WhereClause>,
+) -> TokenStream {
+    quote! {impl #impl_generics std::ops::BitAnd for #name #type_generics #where_clause {
+        type Output = Self;
+
+        fn bitand(self, rhs: Self) -> Self::Output {
+            Self::new(self.x & rhs.x, self.y & rhs.y, self.z & rhs.z)
+        }
+    }
+    }
+}
+
+fn bitor(
     name: &Ident,
     impl_generics: &ImplGenerics,
     type_generics: &TypeGenerics,
     where_clause: &Option<&WhereClause>,
 ) -> TokenStream {
     quote! {
-        impl #impl_generics std::ops::DivAssign for #name #type_generics #where_clause {
-            fn div_assign(&mut self, rhs: #name #type_generics) {
-                *self = self.to_owned() / rhs;
+        impl #impl_generics std::ops::BitOr for #name #type_generics #where_clause {
+            type Output = Self;
+
+            fn bitor(self, rhs: Self) -> Self::Output {
+                Self::new(self.x | rhs.x, self.y | rhs.y, self.z | rhs.z)
+            }
+        }
+    }
+}
+
+fn bitxor(
+    name: &Ident,
+    impl_generics: &ImplGenerics,
+    type_generics: &TypeGenerics,
+    where_clause: &Option<&WhereClause>,
+) -> TokenStream {
+    quote! {
+        impl #impl_generics std::ops::BitXor for #name #type_generics #where_clause {
+            type Output = Self;
+
+            fn bitxor(self, rhs: Self) -> Self::Output {
+                Self::new(self.x ^ rhs.x, self.y ^ rhs.y, self.z ^ rhs.z)
             }
         }
     }
@@ -196,35 +162,63 @@ fn rem(
     }
 }
 
-fn rem_assign(
+fn add_assign(
     name: &Ident,
     impl_generics: &ImplGenerics,
     type_generics: &TypeGenerics,
     where_clause: &Option<&WhereClause>,
-    generic: &GenericParam,
 ) -> TokenStream {
     quote! {
-        impl #impl_generics std::ops::RemAssign<#generic> for #name #type_generics #where_clause {
-            fn rem_assign(&mut self, rhs: #generic) {
-                *self = self.to_owned() % rhs;
+        impl #impl_generics std::ops::AddAssign for #name #type_generics #where_clause {
+            fn add_assign(&mut self, rhs: #name #type_generics) {
+                *self = self.to_owned() + rhs;
             }
         }
     }
 }
 
-fn bitand(
+fn sub_assign(
     name: &Ident,
     impl_generics: &ImplGenerics,
     type_generics: &TypeGenerics,
     where_clause: &Option<&WhereClause>,
 ) -> TokenStream {
-    quote! {impl #impl_generics std::ops::BitAnd for #name #type_generics #where_clause {
-        type Output = Self;
-
-        fn bitand(self, rhs: Self) -> Self::Output {
-            Self::new(self.x & rhs.x, self.y & rhs.y, self.z & rhs.z)
+    quote! {
+        impl #impl_generics std::ops::SubAssign for #name #type_generics #where_clause {
+            fn sub_assign(&mut self, rhs: #name #type_generics) {
+                *self = self.to_owned() - rhs;
+            }
         }
     }
+}
+
+fn mul_assign(
+    name: &Ident,
+    impl_generics: &ImplGenerics,
+    type_generics: &TypeGenerics,
+    where_clause: &Option<&WhereClause>,
+) -> TokenStream {
+    quote! {
+        impl #impl_generics std::ops::MulAssign for #name #type_generics #where_clause {
+            fn mul_assign(&mut self, rhs: #name #type_generics) {
+                *self = self.to_owned() * rhs;
+            }
+        }
+    }
+}
+
+fn div_assign(
+    name: &Ident,
+    impl_generics: &ImplGenerics,
+    type_generics: &TypeGenerics,
+    where_clause: &Option<&WhereClause>,
+) -> TokenStream {
+    quote! {
+        impl #impl_generics std::ops::DivAssign for #name #type_generics #where_clause {
+            fn div_assign(&mut self, rhs: #name #type_generics) {
+                *self = self.to_owned() / rhs;
+            }
+        }
     }
 }
 
@@ -238,23 +232,6 @@ fn bitand_assign(
         impl #impl_generics std::ops::BitAndAssign for #name #type_generics #where_clause {
             fn bitand_assign(&mut self, rhs: Self) {
                 *self = self.to_owned() & rhs;
-            }
-        }
-    }
-}
-
-fn bitor(
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-) -> TokenStream {
-    quote! {
-        impl #impl_generics std::ops::BitOr for #name #type_generics #where_clause {
-            type Output = Self;
-
-            fn bitor(self, rhs: Self) -> Self::Output {
-                Self::new(self.x | rhs.x, self.y | rhs.y, self.z | rhs.z)
             }
         }
     }
@@ -275,23 +252,6 @@ fn bitor_assign(
     }
 }
 
-fn bitxor(
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-) -> TokenStream {
-    quote! {
-        impl #impl_generics std::ops::BitXor for #name #type_generics #where_clause {
-            type Output = Self;
-
-            fn bitxor(self, rhs: Self) -> Self::Output {
-                Self::new(self.x ^ rhs.x, self.y ^ rhs.y, self.z ^ rhs.z)
-            }
-        }
-    }
-}
-
 fn bitxor_assign(
     name: &Ident,
     impl_generics: &ImplGenerics,
@@ -302,6 +262,22 @@ fn bitxor_assign(
         impl #impl_generics std::ops::BitXorAssign for #name #type_generics #where_clause {
             fn bitxor_assign(&mut self, rhs: Self) {
                 *self = self.to_owned() ^ rhs;
+            }
+        }
+    }
+}
+
+fn rem_assign(
+    name: &Ident,
+    impl_generics: &ImplGenerics,
+    type_generics: &TypeGenerics,
+    where_clause: &Option<&WhereClause>,
+    generic: &GenericParam,
+) -> TokenStream {
+    quote! {
+        impl #impl_generics std::ops::RemAssign<#generic> for #name #type_generics #where_clause {
+            fn rem_assign(&mut self, rhs: #generic) {
+                *self = self.to_owned() % rhs;
             }
         }
     }
@@ -324,113 +300,83 @@ fn not(
     }
 }
 
-pub fn generate(
+fn neg(
     ast: &DeriveInput,
     name: &Ident,
     impl_generics: &ImplGenerics,
     type_generics: &TypeGenerics,
     where_clause: &Option<&WhereClause>,
+) -> Option<TokenStream> {
+    match ast.attrs.get(0) {
+        Some(attr) => match attr.path().is_ident("signed") {
+            true => Some(quote! {
+                impl #impl_generics std::ops::Neg for #name #type_generics #where_clause {
+                    type Output = #name #type_generics;
+
+                    fn neg(self) -> Self::Output {
+                        Self::new(-self.x, -self.y, -self.z)
+                    }
+                }
+            }),
+            false => None,
+        },
+        None => None,
+    }
+}
+
+pub fn generate(
+    ast: &DeriveInput,
+    name: &Ident,
+    impl_gnrcs: &ImplGenerics,
+    type_gnrcs: &TypeGenerics,
+    where_clause: &Option<&WhereClause>,
     generic: &GenericParam,
 ) -> TokenStream {
-    let neg = neg(&ast, &name, &impl_generics, &type_generics, &where_clause);
+    let add = add(&name, &impl_gnrcs, &type_gnrcs, &where_clause, &generic);
+    let sub = sub(&name, &impl_gnrcs, &type_gnrcs, &where_clause, &generic);
+    let mul = mul(&name, &impl_gnrcs, &type_gnrcs, &where_clause, &generic);
+    let div = div(&name, &impl_gnrcs, &type_gnrcs, &where_clause, &generic);
+    let bitand = bitand(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
+    let bitor = bitor(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
+    let bitxor = bitxor(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
 
-    let add = add(
-        &name,
-        &impl_generics,
-        &type_generics,
-        &where_clause,
-        &generic,
-    );
+    let rem = rem(&name, &impl_gnrcs, &type_gnrcs, &where_clause, &generic);
 
-    let add_assign = add_assign(&name, &impl_generics, &type_generics, &where_clause);
+    let add_assign = add_assign(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
+    let sub_assign = sub_assign(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
+    let mul_assign = mul_assign(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
+    let div_assign = div_assign(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
+    let bitand_assign = bitand_assign(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
+    let bitor_assign = bitor_assign(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
+    let bitxor_assign = bitxor_assign(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
 
-    let sub = sub(
-        &name,
-        &impl_generics,
-        &type_generics,
-        &where_clause,
-        &generic,
-    );
+    let rem_assign = rem_assign(&name, &impl_gnrcs, &type_gnrcs, &where_clause, &generic);
 
-    let sub_assign = sub_assign(&name, &impl_generics, &type_generics, &where_clause);
-
-    let mul = mul(
-        &name,
-        &impl_generics,
-        &type_generics,
-        &where_clause,
-        &generic,
-    );
-
-    let mul_assign = mul_assign(&name, &impl_generics, &type_generics, &where_clause);
-
-    let div = div(
-        &name,
-        &impl_generics,
-        &type_generics,
-        &where_clause,
-        &generic,
-    );
-
-    let div_assign = div_assign(&name, &impl_generics, &type_generics, &where_clause);
-
-    let rem = rem(
-        &name,
-        &impl_generics,
-        &type_generics,
-        &where_clause,
-        &generic,
-    );
-
-    let rem_assign = rem_assign(
-        &name,
-        &impl_generics,
-        &type_generics,
-        &where_clause,
-        &generic,
-    );
-
-    let not = not(&name, &impl_generics, &type_generics, &where_clause);
-
-    let bitand = bitand(&name, &impl_generics, &type_generics, &where_clause);
-
-    let bitand_assign = bitand_assign(&name, &impl_generics, &type_generics, &where_clause);
-
-    let bitor = bitor(&name, &impl_generics, &type_generics, &where_clause);
-
-    let bitor_assign = bitor_assign(&name, &impl_generics, &type_generics, &where_clause);
-
-    let bitxor = bitxor(&name, &impl_generics, &type_generics, &where_clause);
-
-    let bitxor_assign = bitxor_assign(&name, &impl_generics, &type_generics, &where_clause);
+    let not = not(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
+    let neg = neg(&ast, &name, &impl_gnrcs, &type_gnrcs, &where_clause);
 
     quote! {
-        #neg
-
         #add
-        #add_assign
-
         #sub
-        #sub_assign
-
         #mul
-        #mul_assign
-
         #div
-        #div_assign
+        #bitand
+        #bitor
+        #bitxor
 
         #rem
+
+        #add_assign
+        #sub_assign
+        #mul_assign
+        #div_assign
+        #bitand_assign
+        #bitor_assign
+        #bitxor_assign
+
         #rem_assign
 
         #not
-
-        #bitand
-        #bitand_assign
-
-        #bitor
-        #bitor_assign
-
-        #bitxor
-        #bitxor_assign
+        #neg
     }
 }
