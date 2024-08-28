2a7688f1331afb92445aaaac9e8fed4c40d54991
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-28 20:07:49 +0200

    Slimmed down `Tokens` struct, implemented constructor and getter

--- src/tokens.rs
+++ src/tokens.rs
@@ -1,10 +1,27 @@
 use syn::{DeriveInput, GenericParam, Ident, ImplGenerics, TypeGenerics, WhereClause};
 
-pub struct Tokens<'a> {
-    ast: &'a DeriveInput,
-    name: &'a Ident,
-    impl_generics: ImplGenerics<'a>,
-    type_generics: TypeGenerics<'a>,
-    where_clause: Option<&'a WhereClause>,
-    generic: &'a GenericParam,
+pub struct Tokens {
+    ast: DeriveInput,
+}
+
+impl Tokens {
+    pub fn split(&self) -> (&Ident, ImplGenerics, TypeGenerics, Option<&WhereClause>, &GenericParam) {
+        let name = &self.ast.ident;
+        let (impl_generics, type_generics, where_clause) = self.ast.generics.split_for_impl();
+        let generic = self.ast.generics.params.first().unwrap();
+
+        (
+            name,
+            impl_generics,
+            type_generics,
+            where_clause,
+            generic,
+        )
+    }
+}
+
+impl From<proc_macro::TokenStream> for Tokens {
+    fn from(value: proc_macro::TokenStream) -> Self {
+        Self { ast: syn::parse(value).unwrap() }
+    }
 }
