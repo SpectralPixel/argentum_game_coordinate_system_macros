20d07705db5831af512c77391614148b58da2c25
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-28 20:30:11 +0200

    Replaced inputs to trait generation functions in `arithmetic` with `Tokens`

--- src/arithmetic.rs
+++ src/arithmetic.rs
@@ -1,21 +1,17 @@
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{DeriveInput, GenericParam, Ident, ImplGenerics, TypeGenerics, WhereClause};
+
+use crate::tokens::Tokens;
 
 macro_rules! operation {
     ($trait_name:ident, $func_name:ident, $operation:ident, $operation_failure_message:literal) => {
-        fn $func_name (
-            name: &Ident,
-            impl_generics: &ImplGenerics,
-            type_generics: &TypeGenerics,
-            where_clause: &Option<&WhereClause>,
-            generic: &GenericParam,
-        ) -> TokenStream {
+        fn $func_name (tokens: &Tokens) -> TokenStream {
+            let (_, name, impl_generics, type_generics, where_clause, generic) = tokens.split();
             quote! {
                 impl #impl_generics std::ops::$trait_name for #name #type_generics #where_clause {
                     type Output = Self;
 
-                    fn $func_name(self, rhs: #name #type_generics) -> Self::Output {
+                    fn $func_name(self, rhs: Self) -> Self::Output {
                         let panic_if_out_of_bounds = || panic!($operation_failure_message, self, rhs);
                         let x = #generic::$operation(&self.x, &rhs.x).unwrap_or_else(panic_if_out_of_bounds);
                         let y = #generic::$operation(&self.y, &rhs.y).unwrap_or_else(panic_if_out_of_bounds);
@@ -27,12 +23,8 @@ macro_rules! operation {
         }
     };
     ($trait_name:ident, $func_name:ident, $op:tt) => {
-        fn $func_name(
-            name: &Ident,
-            impl_generics: &ImplGenerics,
-            type_generics: &TypeGenerics,
-            where_clause: &Option<&WhereClause>,
-        ) -> TokenStream {
+        fn $func_name(tokens: &Tokens) -> TokenStream {
+            let (_, name, impl_generics, type_generics, where_clause, _) = tokens.split();
             quote! {
                 impl #impl_generics std::ops::$trait_name for #name #type_generics #where_clause {
                     type Output = Self;
@@ -75,16 +67,11 @@ operation!(BitAnd, bitand, &);
 operation!(BitOr, bitor, |);
 operation!(BitXor, bitxor, ^);
 
-fn rem(
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-    generic: &GenericParam,
-) -> TokenStream {
+fn rem(tokens: &Tokens) -> TokenStream {
+    let (_, name, impl_generics, type_generics, where_clause, generic) = tokens.split();
     quote! {
         impl #impl_generics std::ops::Rem<#generic> for #name #type_generics #where_clause {
-            type Output = #name #type_generics;
+            type Output = Self;
 
             fn rem(self, rhs: #generic) -> Self::Output {
                 Self::new(
@@ -97,12 +84,8 @@ fn rem(
     }
 }
 
-fn add_assign(
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-) -> TokenStream {
+fn add_assign(tokens: &Tokens) -> TokenStream {
+    let (_, name, impl_generics, type_generics, where_clause, _) = tokens.split();
     quote! {
         impl #impl_generics std::ops::AddAssign for #name #type_generics #where_clause {
             fn add_assign(&mut self, rhs: #name #type_generics) {
@@ -112,12 +95,8 @@ fn add_assign(
     }
 }
 
-fn sub_assign(
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-) -> TokenStream {
+fn sub_assign(tokens: &Tokens) -> TokenStream {
+    let (_, name, impl_generics, type_generics, where_clause, _) = tokens.split();
     quote! {
         impl #impl_generics std::ops::SubAssign for #name #type_generics #where_clause {
             fn sub_assign(&mut self, rhs: #name #type_generics) {
@@ -127,12 +106,8 @@ fn sub_assign(
     }
 }
 
-fn mul_assign(
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-) -> TokenStream {
+fn mul_assign(tokens: &Tokens) -> TokenStream {
+    let (_, name, impl_generics, type_generics, where_clause, _) = tokens.split();
     quote! {
         impl #impl_generics std::ops::MulAssign for #name #type_generics #where_clause {
             fn mul_assign(&mut self, rhs: #name #type_generics) {
@@ -142,12 +117,8 @@ fn mul_assign(
     }
 }
 
-fn div_assign(
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-) -> TokenStream {
+fn div_assign(tokens: &Tokens) -> TokenStream {
+    let (_, name, impl_generics, type_generics, where_clause, _) = tokens.split();
     quote! {
         impl #impl_generics std::ops::DivAssign for #name #type_generics #where_clause {
             fn div_assign(&mut self, rhs: #name #type_generics) {
@@ -157,12 +128,8 @@ fn div_assign(
     }
 }
 
-fn bitand_assign(
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-) -> TokenStream {
+fn bitand_assign(tokens: &Tokens) -> TokenStream {
+    let (_, name, impl_generics, type_generics, where_clause, _) = tokens.split();
     quote! {
         impl #impl_generics std::ops::BitAndAssign for #name #type_generics #where_clause {
             fn bitand_assign(&mut self, rhs: Self) {
@@ -172,12 +139,8 @@ fn bitand_assign(
     }
 }
 
-fn bitor_assign(
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-) -> TokenStream {
+fn bitor_assign(tokens: &Tokens) -> TokenStream {
+    let (_, name, impl_generics, type_generics, where_clause, _) = tokens.split();
     quote! {
         impl #impl_generics std::ops::BitOrAssign for #name #type_generics #where_clause {
             fn bitor_assign(&mut self, rhs: Self) {
@@ -187,12 +150,8 @@ fn bitor_assign(
     }
 }
 
-fn bitxor_assign(
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-) -> TokenStream {
+fn bitxor_assign(tokens: &Tokens) -> TokenStream {
+    let (_, name, impl_generics, type_generics, where_clause, _) = tokens.split();
     quote! {
         impl #impl_generics std::ops::BitXorAssign for #name #type_generics #where_clause {
             fn bitxor_assign(&mut self, rhs: Self) {
@@ -202,13 +161,8 @@ fn bitxor_assign(
     }
 }
 
-fn rem_assign(
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-    generic: &GenericParam,
-) -> TokenStream {
+fn rem_assign(tokens: &Tokens) -> TokenStream {
+    let (_, name, impl_generics, type_generics, where_clause, generic) = tokens.split();
     quote! {
         impl #impl_generics std::ops::RemAssign<#generic> for #name #type_generics #where_clause {
             fn rem_assign(&mut self, rhs: #generic) {
@@ -218,15 +172,11 @@ fn rem_assign(
     }
 }
 
-fn not(
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-) -> TokenStream {
+fn not(tokens: &Tokens) -> TokenStream {
+    let (_, name, impl_generics, type_generics, where_clause, _) = tokens.split();
     quote! {
         impl #impl_generics std::ops::Not for #name #type_generics #where_clause {
-            type Output = #name #type_generics;
+            type Output = Self;
 
             fn not(self) -> Self::Output {
                 Self::new(!self.x, !self.y, !self.z)
@@ -235,18 +185,13 @@ fn not(
     }
 }
 
-fn neg(
-    ast: &DeriveInput,
-    name: &Ident,
-    impl_generics: &ImplGenerics,
-    type_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-) -> Option<TokenStream> {
+fn neg(tokens: &Tokens) -> Option<TokenStream> {
+    let (ast, name, impl_generics, type_generics, where_clause, _) = tokens.split();
     match ast.attrs.get(0) {
         Some(attr) => match attr.path().is_ident("signed") {
             true => Some(quote! {
                 impl #impl_generics std::ops::Neg for #name #type_generics #where_clause {
-                    type Output = #name #type_generics;
+                    type Output = Self;
 
                     fn neg(self) -> Self::Output {
                         Self::new(-self.x, -self.y, -self.z)
@@ -259,36 +204,29 @@ fn neg(
     }
 }
 
-pub fn generate(
-    ast: &DeriveInput,
-    name: &Ident,
-    impl_gnrcs: &ImplGenerics,
-    type_gnrcs: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-    generic: &GenericParam,
-) -> TokenStream {
-    let add = add(&name, &impl_gnrcs, &type_gnrcs, &where_clause, &generic);
-    let sub = sub(&name, &impl_gnrcs, &type_gnrcs, &where_clause, &generic);
-    let mul = mul(&name, &impl_gnrcs, &type_gnrcs, &where_clause, &generic);
-    let div = div(&name, &impl_gnrcs, &type_gnrcs, &where_clause, &generic);
-    let bitand = bitand(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
-    let bitor = bitor(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
-    let bitxor = bitxor(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
+pub fn generate(tokens: &Tokens) -> TokenStream {
+    let add = add(&tokens);
+    let sub = sub(&tokens);
+    let mul = mul(&tokens);
+    let div = div(&tokens);
+    let bitand = bitand(&tokens);
+    let bitor = bitor(&tokens);
+    let bitxor = bitxor(&tokens);
 
-    let rem = rem(&name, &impl_gnrcs, &type_gnrcs, &where_clause, &generic);
+    let rem = rem(&tokens);
 
-    let add_assign = add_assign(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
-    let sub_assign = sub_assign(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
-    let mul_assign = mul_assign(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
-    let div_assign = div_assign(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
-    let bitand_assign = bitand_assign(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
-    let bitor_assign = bitor_assign(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
-    let bitxor_assign = bitxor_assign(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
+    let add_assign = add_assign(&tokens);
+    let sub_assign = sub_assign(&tokens);
+    let mul_assign = mul_assign(&tokens);
+    let div_assign = div_assign(&tokens);
+    let bitand_assign = bitand_assign(&tokens);
+    let bitor_assign = bitor_assign(&tokens);
+    let bitxor_assign = bitxor_assign(&tokens);
 
-    let rem_assign = rem_assign(&name, &impl_gnrcs, &type_gnrcs, &where_clause, &generic);
+    let rem_assign = rem_assign(&tokens);
 
-    let not = not(&name, &impl_gnrcs, &type_gnrcs, &where_clause);
-    let neg = neg(&ast, &name, &impl_gnrcs, &type_gnrcs, &where_clause);
+    let not = not(&tokens);
+    let neg = neg(&tokens);
 
     quote! {
         #add
