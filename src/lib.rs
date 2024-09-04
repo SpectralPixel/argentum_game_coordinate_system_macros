32fddc191e9527ab6eda5d9d4856aba96993c195
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-09-04 19:08:11 +0200

    Renamed `Coordinate` derive to `CoordinateArithmetic`

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use tokens::Tokens;
 mod arithmetic;
 mod tokens;
 
-#[proc_macro_derive(Coordinate, attributes(signed))]
+#[proc_macro_derive(CoordinateArithmetic, attributes(signed))]
 pub fn coordinate_trait_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let tokens = Tokens::from(input);
     let arithmetic = arithmetic::generate(&tokens);
