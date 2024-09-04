026b575ad71dae319f51a94ae36357e3215dcc7a
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-09-04 19:18:29 +0200

    Removed attribute from `CoordinateArithmetic` Since `Neg` is no longes being implemented in this macro, the implementations are valid for both signed and unsigned generics.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use tokens::Tokens;
 mod arithmetic;
 mod tokens;
 
-#[proc_macro_derive(CoordinateArithmetic, attributes(signed))]
+#[proc_macro_derive(CoordinateArithmetic)]
 pub fn coordinate_arithmetic_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let tokens = Tokens::from(input);
     let arithmetic = arithmetic::generate(&tokens);
