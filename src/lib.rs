42fb65546cb9247dd05e0f4f9e6af1855a8162d2
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-09-04 19:09:57 +0200

    Rename proc-macro function to `coordinate_arithmetic_derive`

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ mod arithmetic;
 mod tokens;
 
 #[proc_macro_derive(CoordinateArithmetic, attributes(signed))]
-pub fn coordinate_trait_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn coordinate_arithmetic_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let tokens = Tokens::from(input);
     let arithmetic = arithmetic::generate(&tokens);
     arithmetic.into()
