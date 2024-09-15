638de3b14619e88c318dc5e78c052e714bd25fe2
Emil Engelbert <emil.engelbert@gmail.com>
2024-09-15 11:59:55 +0200

    Revert "Commented out unused `coordinate_arithmetic_derive` for now"
    This reverts commit ff144ea7565d7c7ebed4d98994d57e92aa146cf0.

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@ use tokens::Tokens;
 mod arithmetic;
 mod tokens;
 
-// #[proc_macro_derive(CoordinateArithmetic)]
-// pub fn coordinate_arithmetic_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-//     let tokens = Tokens::from(input);
-//     let arithmetic = arithmetic::generate(&tokens);
-//     arithmetic.into()
-// }
+#[proc_macro_derive(CoordinateArithmetic)]
+pub fn coordinate_arithmetic_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let tokens = Tokens::from(input);
+    let arithmetic = arithmetic::generate(&tokens);
+    arithmetic.into()
+}
 
 #[proc_macro]
 pub fn generate_arithmetic_implementation(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
