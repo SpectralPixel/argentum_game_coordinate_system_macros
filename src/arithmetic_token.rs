28564dfcd64931db37f9bd9c77e994396893b8ba
Emil Engelbert <emil.engelbert@gmail.com>
2024-09-15 11:26:17 +0200

    I  JUST MADE THE SAME CHANGES A BILLION FUCKING PTIMES JUST TO REVERT ONE DAMN COMMIT TAHT DIDNT WANT TO. FUCK YOU GIT, FUCK YOU TORSVALDS, FUCK YOU COMMIT THAT DIDNT WANT TO REVERT AND CAUSED ME TO IMPLEMENT **THE SAME FUCKING COMMIT 5 TIMES** DUE TO YOUR SHITTY MERGES. sorry about this
    This reverts commit 10bacacfd3177558a735f173064596b2b43f0bb8.

--- src/arithmetic_token.rs
+++ src/arithmetic_token.rs
@@ -1,41 +1,5 @@
-use std::sync::OnceLock;
-
-use bimap::BiHashMap;
-
 struct ArithmeticToken;
 
 impl ArithmeticToken {
-    fn token_name_trait_name_conversions() -> &'static BiHashMap<&'static str, &'static str> {
-        static BI_HASHMAP: OnceLock<BiHashMap<&str, &str>> = OnceLock::new();
-        BI_HASHMAP.get_or_init(|| {
-            let mut conversions = BiHashMap::new();
-            conversions.insert("And", "BitAnd");
-            conversions.insert("AndEq", "BitAndAssign");
-            conversions.insert("Caret", "BitXor");
-            conversions.insert("CaretEq", "BitXorAssign");
-            conversions.insert("Sub", "Sub");
-            conversions.insert("SubEq", "SubAssign");
-            conversions.insert("Not", "Not");
-            conversions.insert("Or", "BitOr");
-            conversions.insert("OrEq", "BitOrAssign");
-            conversions.insert("Percent", "Rem");
-            conversions.insert("PercentEq", "RemAssign");
-            conversions.insert("Plus", "Add");
-            conversions.insert("PlusEq", "AddAssign");
-            conversions.insert("Shl", "Shl");
-            conversions.insert("ShlEq", "ShlAssign");
-            conversions.insert("Shr", "Shr");
-            conversions.insert("ShrEq", "ShrAssign");
-            conversions.insert("Slash", "Div");
-            conversions.insert("SlashEq", "DivAssign");
-            conversions.insert("Star", "Mul");
-            conversions.insert("StarEq", "MulAssign");
-        })
-    }
-}
-
-macro_rules! arithmetic_token_wrapper_struct {
-    () => {
-        
-    };
+    
 }
